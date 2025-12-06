288a0872b0a0562baca08d0a28d99c975abe1ec4
rehanmirza76 <abdulrehan0317@gmail.com>
2025-12-06 23:25:56 +0500

    updated

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Physical AI & Humanoid Robotics',
   tagline: 'An Open-Source Book',
-  favicon: '',
+  favicon: 'static/img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
